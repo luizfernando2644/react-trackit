7cd001738a7f85c5a5840837994d00b666b53ce6
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-10-02 16:24:22 -0300

    Criado os contextos da aplicacao

--- src/pages/Habitos/Habitos.jsx
+++ src/pages/Habitos/Habitos.jsx
@@ -1,11 +1,29 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import styled from "styled-components";
 
 export default function Habitos() {
     return (
         <div>
             <Header />
+            <Content>
+                <p>Meus Hábitos</p>
+                <button>+</button>
+                <HabitsContainer>
+                    <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+                </HabitsContainer>
+            </Content>
             <Footer />
         </div>
     )
 }
+
+const Content = styled.div`
+    height: 80vh;
+    margin-top: 85px;
+    background-color: #F2F2F2;
+`
+
+const HabitsContainer = styled.div`
+
+`
